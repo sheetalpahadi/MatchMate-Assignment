db316ad970b877de941ad0695b38b3b50dd8f64d
sheetalpahadi <sheetal992009@gmail.com>
2025-07-14 16:56:43 +0530

    Initial Commit

---
+++ MatchMate-Assignment/MatchMate_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchMate_AssignmentApp.swift
+//  MatchMate-Assignment
+//
+//  Created by Admin on 14/07/25.
+//
+
+import SwiftUI
+
+@main
+struct MatchMate_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
